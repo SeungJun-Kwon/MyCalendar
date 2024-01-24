3c2a1c486f8f1289865a5d187109157c1e8a4b43
DAchoMI <tyrnsorl123@naver.com>
2024-01-24 20:35:17 +0900

    :sparkles: ResponseEntity를 활용해 Http 상태에 따른 처리

--- src/main/java/com/sparta/myschedules/controller/ScheduleController.java
+++ src/main/java/com/sparta/myschedules/controller/ScheduleController.java
@@ -2,8 +2,11 @@ package com.sparta.myschedules.controller;
 
 import com.sparta.myschedules.dto.ScheduleRequestDto;
 import com.sparta.myschedules.dto.ScheduleResponseDto;
+import com.sparta.myschedules.entity.ResponseMessage;
 import com.sparta.myschedules.service.ScheduleService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,9 +28,27 @@ public class ScheduleController {
     }
 
     @GetMapping("/{id}")
-    public ScheduleResponseDto getScheduleById(@PathVariable Long id) {
-        return scheduleService.getScheduleById(id);
+    public ResponseEntity<ResponseMessage<?>> getScheduleById(@PathVariable Long id) {
+        ScheduleResponseDto dto;
+
+        try {
+            dto = scheduleService.getScheduleById(id);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().
+                    body(ResponseMessage.builder()
+                            .msg(id + " 유저 탐색 실패")
+                            .httpCode(HttpStatus.BAD_REQUEST.value())
+                            .data(null)
+                            .build());
+        }
+
+        return ResponseEntity.ok(ResponseMessage.builder()
+                .msg(id + " 유저 탐색 완료")
+                .httpCode(HttpStatus.OK.value())
+                .data(dto)
+                .build());
     }
+
 
     @PutMapping("/{id}/{password}")
     public Long updateSchedule(@PathVariable Long id, @PathVariable String password, @RequestBody ScheduleRequestDto requestDto) {
